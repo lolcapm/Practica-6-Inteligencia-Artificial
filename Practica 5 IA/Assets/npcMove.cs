54f21ee598f69d362946c4733f01725221f5fa0e
lolcapm <amayberc@gmail.com>
2018-09-04 21:05:53 -0400

    Practica 6 IA

---
+++ Practica 5 IA/Assets/npcMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcMove : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField]
+	Transform _destination;
+	UnityEngine.AI.NavMeshAgent _navMeshAgent;
+	void Start () {
+		_navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		if (_navMeshAgent == null) {
+			Debug.LogError ("El agente de navegacion no esta anadido" + gameObject.name);
+		} 
+		else 
+		{
+			SetDestination ();
+		}
+	}
+	private void SetDestination()
+	{
+		Vector3 targetVector = _destination.transform.position;
+		_navMeshAgent.SetDestination (targetVector);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
